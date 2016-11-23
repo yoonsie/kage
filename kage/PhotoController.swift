ca5dd02feaeedd357806ac41d3f7ae5e498e0f26
Yoonsie Kim <yoonsie.kim@gmail.com>
2016-11-22 21:34:27 -0500

    changes

---
+++ kage/PhotoController.swift
@@ -0,0 +1,23 @@
+//
+//  PhotoController.swift
+//  kage
+//
+//  Created by Yoonsie Kim on 11/22/16.
+//  Copyright © 2016 Yoonsie Kim. All rights reserved.
+//
+
+import UIKit
+
+class PhotoController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
